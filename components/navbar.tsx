7379ee32dc37d0133869239ec6e4aca49a8fc87e
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-07-06 17:32:06 -1100

    feat: added dynamic metadata, open graphs and twitter summaries

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,16 +5,35 @@ import getCategories from "@/actions/get-categories";
 import NavbarActions from "@/components/navbar-actions";
 
 const Navbar = async () => {
-
-    const categories = await getCategories();
+  const categories = await getCategories();
   return (
     <div className="border-b">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
-          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">STORE</p>
+          <Link
+            aria-aria-label="Nike home page"
+            href="/"
+            className="ml-4 h-[59px] w-[59px] flex lg:ml-0 gap-x-2"
+          >
+            <svg
+              aria-hidden="true"
+              className="h-full w-full transition-transform scale-[1.33]"
+              focusable="false"
+              viewBox="0 0 24 24"
+              role="img"
+              width="24px"
+              height="24px"
+              fill="none"
+            >
+              <path
+                fill="currentColor"
+                fill-rule="evenodd"
+                d="M21 8.719L7.836 14.303C6.74 14.768 5.818 15 5.075 15c-.836 0-1.445-.295-1.819-.884-.485-.76-.273-1.982.559-3.272.494-.754 1.122-1.446 1.734-2.108-.144.234-1.415 2.349-.025 3.345.275.2.666.298 1.147.298.386 0 .829-.063 1.316-.19L21 8.719z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
           </Link>
-          <MainNav data={categories}/>
+          <MainNav data={categories} />
           <NavbarActions />
         </div>
       </Container>
