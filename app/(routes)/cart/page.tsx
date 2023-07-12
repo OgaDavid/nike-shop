96c73071ef3788184219cc97e180244e433fde04
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-07-12 04:54:33 -1100

    feat: added order summary, added checkout functionality

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -6,6 +6,7 @@ import Container from "@/components/ui/container";
 import useCart from "@/hooks/use-cart";
 
 import CartItem from "./components/cart-item";
+import Summary from "./components/summary";
 
 export const revalidate = 0;
 
@@ -37,6 +38,7 @@ const CartPage = () => {
                 ))}
               </ul>
             </div>
+            <Summary />
           </div>
         </div>
       </Container>
