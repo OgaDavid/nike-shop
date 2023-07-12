52b16b325549ca49fb30407530ccd8faa5fa9b5c
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-07-12 05:17:52 -1100

    fix: add to cart funuctionality not working from dynamic product page

--- components/info.tsx
+++ components/info.tsx
@@ -1,15 +1,24 @@
-"use client"
+"use client";
 
 import { Product } from "@/types";
 import Currency from "./ui/currency";
 import Button from "./ui/button";
-import { ShoppingCart } from 'lucide-react'
+import { ShoppingCart } from "lucide-react";
+import useCart from "@/hooks/use-cart";
+import { MouseEventHandler } from "react";
 
 interface InfoProps {
   data: Product;
 }
 
 const Info: React.FC<InfoProps> = ({ data }) => {
+  const cart = useCart();
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    cart.addItem(data);
+  };
   return (
     <div>
       <h1 className="text-2xl sm:text-3xl sm:font-bold text-gray-900">
@@ -35,10 +44,10 @@ const Info: React.FC<InfoProps> = ({ data }) => {
           />
         </div>
       </div>
-      <div className='mt-10 flex items-center gap-x-3'>
-        <Button className='flex items-center gap-x-2'>
-            Add to Cart
-            <ShoppingCart />
+      <div className="mt-10 flex items-center gap-x-3">
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
+          Add to Cart
+          <ShoppingCart />
         </Button>
       </div>
     </div>
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,22 +1,28 @@
 "use client";
 
-import { Product } from "@/types";
 import Image from "next/image";
-import IconButton from "@/components/ui/icon-button";
-import { Expand, ShoppingCart } from "lucide-react";
-import Currency from "./currency";
-import { useRouter } from "next/navigation";
 import { MouseEventHandler } from "react";
+import { Expand, ShoppingCart } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import Currency from "@/components/ui/currency";
+import IconButton from "@/components/ui/icon-button";
 import usePreviewModal from "@/hooks/use-preview-modal";
 import useCart from "@/hooks/use-cart";
+import { Product } from "@/types";
 
-interface ProductCardProps {
+interface ProductCard {
   data: Product;
 }
 
-const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+const ProductCard: React.FC<ProductCard> = ({ data }) => {
   const previewModal = usePreviewModal();
   const cart = useCart();
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`);
+  };
 
   const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
     event.stopPropagation();
@@ -24,29 +30,24 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
     previewModal.onOpen(data);
   };
 
-  const addToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
     event.stopPropagation();
 
     cart.addItem(data);
   };
 
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push(`/product/${data?.id}`);
-  };
   return (
     <div
       onClick={handleClick}
       className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
     >
+      {/* Image & actions */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image
-          alt="image"
-          src={data?.images?.[0].url}
+          src={data.images?.[0]?.url}
+          alt=""
           fill
-          objectFit="contain"
-          className="object-cover rounded-md aspect-square"
+          className="aspect-square object-cover rounded-md"
         />
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
@@ -55,16 +56,18 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
-              onClick={addToCart}
+              onClick={onAddToCart}
               icon={<ShoppingCart size={20} className="text-gray-600" />}
             />
           </div>
         </div>
       </div>
+      {/* Description */}
       <div>
-        <p>{data.name}</p>
-        <p className="text-[12px] text-gray-500">{data.category?.name}</p>
+        <p className="font-semibold text-lg">{data.name}</p>
+        <p className="text-sm text-gray-500">{data.category?.name}</p>
       </div>
+      {/* Price & Reiew */}
       <div className="flex items-center justify-between">
         <Currency value={data?.price} />
       </div>
