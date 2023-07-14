6552937982b16dc84b24283f4f489792ff6f3d29
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-07-14 08:21:39 -1100

    fix: reduce related items text

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -10,7 +10,7 @@ interface ProductListProps {
 const ProductList: React.FC<ProductListProps> = ({ title, items }) => {
   return (
     <div className="space-y-4">
-      <h3 className="font-bold text-3xl">{title}</h3>
+      <h3 className="font-bold text-xl sm:text-3xl">{title}</h3>
       { items.length === 0 && <NoResults /> }
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {items.map((item) => (
